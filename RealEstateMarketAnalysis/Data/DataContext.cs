527af582328f1650dcc0f3721f0dcfaf9c1860d3
ArtOvchinnickov <User@DESKTOP-0JI8E8N>
2025-04-11 22:01:36 +0300

    Изменения 1

---
+++ RealEstateMarketAnalysis/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RealEstateMarketAnalysis.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserModel> Users => Set<UserModel>();
+    }
+}
